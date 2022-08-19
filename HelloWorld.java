945fc329c96f820fda9a03111ddd7d85fa90f587
loira <iingridribeiroo09@gmail.com>
2022-08-19 15:42:53 -0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// metódo MAIN
+// em Java as coisas são todas definidas em classes!
+// JAVA SEMPRE CAMELCASE
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
